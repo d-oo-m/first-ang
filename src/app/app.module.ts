e12f89db1e99abd691af95386265c925f45fd49f
d-oo-m <nazar.ilchuk@gmail.com>
2018-02-10 13:10:41 +0200

    section1 and 2 progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products.component';
+import { ProductComponent } from './product/product.component';
+import { ProductsService } from './products.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    ProductComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [ProductsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
